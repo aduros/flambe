5d8f12cf9744d3ca48bc2840d176217bf2ee5f01
Bruno Garcia <ah.duros@gmail.com>
2011-12-11 05:19:20 -0800

    Node tweaks.

--- src/flambe/server/Node.hx
+++ src/flambe/server/Node.hx
@@ -4,15 +4,19 @@
 
 package flambe.server;
 
+// TODO(bruno): Use one of the nodejs externs on haxelib
 class Node
 {
+    public static var require (_require, null) :String -> Dynamic;
+    public static var console (_console, null) :Dynamic;
+    public static var process (_process, null) :Dynamic;
+
     public static var stringify :Dynamic -> String = untyped JSON.stringify;
     public static var parse :String -> Dynamic = untyped JSON.parse;
-    public static var require :String -> Dynamic = untyped __js__("require");
 
     public static function log (message :String)
     {
-        untyped console.log(message);
+        console.log(message);
     }
 
     inline public static function newBuffer (?data :String, ?encoding :String) :Dynamic
@@ -25,4 +29,8 @@ class Node
     {
         throw untyped __js__("new Error")(message);
     }
+
+    inline private static function _require () return untyped __js__("require")
+    inline private static function _console () return untyped __js__("console")
+    inline private static function _process () return untyped __js__("process")
 }

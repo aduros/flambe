fb35dec6ed4e2b47df9a8b55a4c8978e73d3458b
Bruno Garcia <ah.duros@gmail.com>
2012-01-23 20:37:55 -0800

    Add MultiPromise for handling multiple promises coherently.
    var promise = new MultiPromise([
        System.loadAssetPack(Manifest.build("foo")),
        System.loadAssetPack(Manifest.build("bar")),
        System.loadAssetPack(Manifest.build("baz")),
    ]);
    promise.get(function (results) {
        // Called when all 3 asset packs have finished loading
        var foo :AssetPack = results[0];
        var bar :AssetPack = results[1];
        var baz :AssetPack = results[2];
    });

---
+++ src/flambe/util/MultiPromise.hx
@@ -0,0 +1,77 @@
+//
+// Flambe - Rapid game development
+// https://github.com/aduros/flambe/blob/master/LICENSE.txt
+
+package flambe.util;
+
+import flambe.Disposer;
+
+/**
+ * Multiplexes a list of promises into a single promise. When all promises have been fulfilled, the
+ * success signal will be emitted with an array containing their respective results. Progress and
+ * error signals are also aggregated.
+ */
+class MultiPromise extends Promise<Array<Dynamic>>
+{
+    public function new (promises :Array<Dynamic>)
+    {
+        super();
+        _promises = cast promises.copy();
+        _successCount = 0;
+        _disposer = new Disposer();
+        for (promise in _promises) {
+            _disposer.connect1(promise.success, onSuccess);
+            _disposer.connect1(promise.error, onError);
+            _disposer.connect0(promise.progressChanged, onProgressChanged);
+        }
+    }
+
+    private function onSuccess (_)
+    {
+        if (_promises == null) {
+            return;
+        }
+        ++_successCount;
+        if (_successCount >= _promises.length) {
+            var results = [];
+            for (promise in _promises) {
+                results.push(promise.result);
+            }
+            finalize();
+            setResult(cast results);
+        }
+    }
+
+    private function onError (message :String)
+    {
+        if (_promises == null) {
+            return;
+        }
+        finalize();
+        error.emit(message);
+    }
+
+    private function onProgressChanged ()
+    {
+        if (_promises == null) {
+            return;
+        }
+        _progress = 0;
+        _total = 0;
+        for (promise in _promises) {
+            _progress += promise.progress;
+            _total += promise.total;
+        }
+        progressChanged.emit();
+    }
+
+    private function finalize ()
+    {
+        _disposer.dispose();
+        _promises = null; // Marks that all promises are fulfilled, or there was an error
+    }
+
+    private var _promises :Array<Promise<Dynamic>>;
+    private var _successCount :Int;
+    private var _disposer :Disposer;
+}

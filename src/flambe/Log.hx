6e350aa9c15ad4dd97a58ab098fc1a045465a776
Bruno Garcia <b@aduros.com>
2012-02-27 23:01:36 -0800

    No need for this hack anymore.

--- src/flambe/Log.hx
+++ src/flambe/Log.hx
@@ -8,15 +8,5 @@ import flambe.util.Logger;
 
 class Log
 {
-    // public static var log = System.logger("flambe");
-
-    // Normally you'd just be able to write the above, but since this logger is used internally, we
-    // avoid System here due to static initializer dependency issues.
-    public static var log = new Logger(
-#if flash
-        flambe.platform.flash.FlashAppDriver.instance
-#else
-        flambe.platform.html.HtmlAppDriver.instance
-#end
-        .createLogHandler("flambe"));
+    public static var log = System.logger("flambe");
 }

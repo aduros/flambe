0efaefbdadd0449b098437fceab4c2fb626f8778
Bruno Garcia <b@aduros.com>
2012-05-27 14:50:58 -0700

    Add Hashes.getBool.

--- src/flambe/util/Hashes.hx
+++ src/flambe/util/Hashes.hx
@@ -7,7 +7,8 @@ package flambe.util;
 class Hashes
 {
     /**
-     * Fetch a string from a dynamic Hash, or a default value if one doesn't exist.
+     * Fetch a string from a hash, converting it from another type if necessary.
+     * @param defaultValue The value to return if the key was not found.
      */
     public static function getString (
         hash :Hash<Dynamic>, key :String, defaultValue :String = null) :String
@@ -16,6 +17,10 @@ class Hashes
         return (value != null) ? ""+value : defaultValue;
     }
 
+    /**
+     * Fetch a float from a hash, converting it from another type if necessary.
+     * @param defaultValue The value to return if the key was not found.
+     */
     public static function getFloat (
         hash :Hash<Dynamic>, key :String, defaultValue :Float = 0) :Float
     {
@@ -23,10 +28,25 @@ class Hashes
         return (value != null) ? Std.parseFloat(""+value) : defaultValue;
     }
 
+    /**
+     * Fetch a integer from a hash, converting it from another type if necessary.
+     * @param defaultValue The value to return if the key was not found.
+     */
     public static function getInt (
-        hash :Hash<Dynamic>, key :String, defaultValue :Int = 0) :Float
+        hash :Hash<Dynamic>, key :String, defaultValue :Int = 0) :Int
     {
         var value = hash.get(key);
         return (value != null) ? Std.parseInt(""+value) : defaultValue;
+    }
+
+    /**
+     * Fetch a boolean from a hash, converting it from another type if necessary.
+     * @param defaultValue The value to return if the key was not found.
+     */
+    public static function getBool (
+        hash :Hash<Dynamic>, key :String, defaultValue :Bool = false) :Bool
+    {
+        var value = hash.get(key);
+        return (value != null) ? value != 0 && value != false && value != "false" : defaultValue;
     }
 }

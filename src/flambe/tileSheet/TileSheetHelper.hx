8eb0284024103be7e7230cf1605a500a1dfd0e45
sonygod <sonygodx@163.com>
2012-09-09 13:04:18 +0800

    tileSheet work

--- src/flambe/tileSheet/TileSheetHelper.hx
+++ src/flambe/tileSheet/TileSheetHelper.hx
@@ -4,20 +4,23 @@ package flambe.tileSheet;
 import flambe.Component;
 import flash.display.BitmapData;
 import haxe.Json;
+   import flambe.tileSheet.Format ;
+
 class TileSheetHelper extends Component {
 
 
     public function new ():Void{
-        super();
+
 
     }
-    public function prepareAnimTexture(src:String):AnimTextureSheet {
-        var seqRaw:Dynamic = Json.parse(pack.loadFile(src));
+    public function prepareAnimTexture(data:String):AnimTextureSheet {
+        var seqRaw:Dynamic = Json.parse(data);
 
         var animData:Array<FrameData> = new Array<FrameData>();
         populateFrameArray(animData, seqRaw.frames);
 
-        animData.sortOn("id");
+       // animData.sortOn("id");
+      //  animData.so
 
         var tileSheet:AnimTextureSheet = new AnimTextureSheet();
         tileSheet.init(animData);
@@ -27,7 +30,7 @@ class TileSheetHelper extends Component {
     function populateFrameArray(arDest:Array<FrameData>, src:Dynamic):Void {
         var entry:SheetFormat;
         var nameId:String;
-        var item:SheetFormat;
+        var item:FrameData;
         for (keyName in Reflect.fields(src)) {
             nameId = keyName.substring(0, keyName.lastIndexOf(".png"));
             entry = Reflect.field(src, keyName);

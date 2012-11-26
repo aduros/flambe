0dac0a1192a157fec225efbdb79fb901dd04f897
Bruno Garcia <b@aduros.com>
2012-11-25 18:33:49 -0800

    Document some methods.

--- src/flambe/display/Graphics.hx
+++ src/flambe/display/Graphics.hx
@@ -4,21 +4,51 @@
 
 package flambe.display;
 
+/**
+ * Draws to a surface.
+ */
 interface Graphics
 {
+    /**
+     * Saves the graphics state until the next restore(). The state contains the transformation
+     * matrix, alpha, and blend mode.
+     */
     function save () :Void;
+
+    /** Translates the transformation matrix. */
     function translate (x :Float, y :Float) :Void;
+
+    /** Scales the transformation matrix. */
     function scale (x :Float, y :Float) :Void;
+
+    /** Rotates the transformation matrix by the given angle, in degrees. */
     function rotate (rotation :Float) :Void;
+
+    /** Multiplies the transformation matrix by the given matrix. */
     function transform (m00 :Float, m10 :Float, m01 :Float, m11 :Float, m02 :Float, m12 :Float) :Void;
+
+    /** Multiplies the alpha by the given factor. */
     function multiplyAlpha (factor :Float) :Void;
+
+    /** Sets the alpha to use for drawing. */
     function setAlpha (alpha :Float) :Void;
+
+    /** Sets the blend mode to use for drawing. */
     function setBlendMode (blendMode :BlendMode) :Void;
+
+    /** Restores the graphics state back to the previous save(). */
     function restore () :Void;
 
+    /** Draws a texture at the given point. */
     function drawImage (texture :Texture, destX :Float, destY :Float) :Void;
+
+    /** Draws a texture sub-region at the given point. */
     function drawSubImage (texture :Texture, destX :Float, destY :Float,
         sourceX :Float, sourceY :Float, sourceW :Float, sourceH :Float) :Void;
+
+    /** Draws a repeating texture to the given region. */
     function drawPattern (texture :Texture, destX :Float, destY :Float, width :Float, height :Float) :Void;
+
+    /** Draws a colored rectangle at the given region. */
     function fillRect (color :Int, x :Float, y :Float, width :Float, height :Float) :Void;
 }
--- src/flambe/display/Texture.hx
+++ src/flambe/display/Texture.hx
@@ -22,11 +22,31 @@ interface Texture
     var height (getHeight, null) :Int;
 
     /**
-     * The Graphics that draws to this texture.
+     * <p>The Graphics that draws to this texture.</p>
+     *
+     * <p>NOTE: In Flash/AIR, this API is currently experimental. Stage3D forces render targets to
+     * be cleared before first rendering, so this may cause the texture to be cleared. If you need
+     * to support Stage3D, consider keeping a second cache texture and flipping between them.</p>
      */
     var graphics (getGraphics, null) :Graphics;
 
+    /**
+     * Reads pixels out from the given region. This is potentially a very SLOW operation, avoid
+     * overusing it.
+     *
+     * @returns A byte buffer in RGBA order.
+     */
     function readPixels (x :Int, y :Int, width :Int, height :Int) :Bytes;
 
+    /**
+     * <p>Writes pixels at a given position. sourceW/H is the width and height of the given byte
+     * buffer. This is potentially a very SLOW operation, avoid overusing it.</p>
+     *
+     * <p>NOTE: In Flash/AIR, this API is currently experimental. Stage3D forces render targets to
+     * be cleared before first rendering, so this may cause the texture to be cleared. If you need
+     * to support Stage3D, consider keeping a second cache texture and flipping between them.</p>
+     *
+     * @param pixels A byte buffer in RGBA order.
+     */
     function writePixels (pixels :Bytes, x :Int, y :Int, sourceW :Int, sourceH :Int) :Void;
 }

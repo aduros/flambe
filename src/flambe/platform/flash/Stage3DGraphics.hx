1a4fe7fc9f8282fe3639edb1df8ddb0f1bc9702a
Bruno Garcia <b@aduros.com>
2012-12-04 00:09:14 -0800

    Pull the quad transformation out into a function.

--- src/flambe/platform/flash/Stage3DGraphics.hx
+++ src/flambe/platform/flash/Stage3DGraphics.hx
@@ -102,60 +102,38 @@ class Stage3DGraphics
         var texture = Lib.as(texture, Stage3DTexture);
         var state = getTopState();
 
-        var x1 = destX;
-        var y1 = destY;
-        var x2 = x1 + sourceW;
-        var y2 = y1 + sourceH;
-        var scratch = _scratchQuadVector;
-
-        scratch[0] = x1;
-        scratch[1] = y1;
-        // scratch[2] = 0;
-
-        scratch[3] = x2;
-        scratch[4] = y1;
-        // scratch[5] = 0;
-
-        scratch[6] = x2;
-        scratch[7] = y2;
-        // scratch[8] = 0;
-
-        scratch[9] = x1;
-        scratch[10] = y2;
-        // scratch[11] = 0;
-
-        state.matrix.transformVectors(scratch, scratch);
-
-        var offset = _batcher.prepareDrawImage(_renderTarget, state.blendMode, texture);
-        var data = _batcher.data;
-        var alpha = state.alpha;
+        var pos = transformQuad(destX, destY, sourceW, sourceH);
         var w = texture.width;
         var h = texture.height;
         var u1 = texture.maxU*sourceX / w;
         var v1 = texture.maxV*sourceY / h;
         var u2 = texture.maxU*(sourceX + sourceW) / w;
         var v2 = texture.maxV*(sourceY + sourceH) / h;
+        var alpha = state.alpha;
 
-        data[  offset] = scratch[0];
-        data[++offset] = scratch[1];
+        var offset = _batcher.prepareDrawImage(_renderTarget, state.blendMode, texture);
+        var data = _batcher.data;
+
+        data[  offset] = pos[0];
+        data[++offset] = pos[1];
         data[++offset] = u1;
         data[++offset] = v1;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[3];
-        data[++offset] = scratch[4];
+        data[++offset] = pos[3];
+        data[++offset] = pos[4];
         data[++offset] = u2;
         data[++offset] = v1;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[6];
-        data[++offset] = scratch[7];
+        data[++offset] = pos[6];
+        data[++offset] = pos[7];
         data[++offset] = u2;
         data[++offset] = v2;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[9];
-        data[++offset] = scratch[10];
+        data[++offset] = pos[9];
+        data[++offset] = pos[10];
         data[++offset] = u1;
         data[++offset] = v2;
         data[++offset] = alpha;
@@ -165,54 +143,35 @@ class Stage3DGraphics
     {
         var texture = Lib.as(texture, Stage3DTexture);
         var state = getTopState();
-        var x2 = x + width;
-        var y2 = y + height;
-        var scratch = _scratchQuadVector;
 
-        scratch[0] = x;
-        scratch[1] = y;
-        // scratch[2] = 0;
-
-        scratch[3] = x2;
-        scratch[4] = y;
-        // scratch[5] = 0;
-
-        scratch[6] = x2;
-        scratch[7] = y2;
-        // scratch[8] = 0;
-
-        scratch[9] = x;
-        scratch[10] = y2;
-        // scratch[11] = 0;
-
-        state.matrix.transformVectors(scratch, scratch);
-
-        var offset = _batcher.prepareDrawPattern(_renderTarget, state.blendMode, texture);
-        var data = _batcher.data;
+        var pos = transformQuad(x, y, width, height);
         var u2 = texture.maxU * (width / texture.width);
         var v2 = texture.maxV * (height / texture.height);
         var alpha = state.alpha;
 
-        data[  offset] = scratch[0];
-        data[++offset] = scratch[1];
+        var offset = _batcher.prepareDrawPattern(_renderTarget, state.blendMode, texture);
+        var data = _batcher.data;
+
+        data[  offset] = pos[0];
+        data[++offset] = pos[1];
         data[++offset] = 0;
         data[++offset] = 0;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[3];
-        data[++offset] = scratch[4];
+        data[++offset] = pos[3];
+        data[++offset] = pos[4];
         data[++offset] = u2;
         data[++offset] = 0;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[6];
-        data[++offset] = scratch[7];
+        data[++offset] = pos[6];
+        data[++offset] = pos[7];
         data[++offset] = u2;
         data[++offset] = v2;
         data[++offset] = alpha;
 
-        data[++offset] = scratch[9];
-        data[++offset] = scratch[10];
+        data[++offset] = pos[9];
+        data[++offset] = pos[10];
         data[++offset] = 0;
         data[++offset] = v2;
         data[++offset] = alpha;
@@ -221,58 +180,39 @@ class Stage3DGraphics
     public function fillRect (color :Int, x :Float, y :Float, width :Float, height :Float)
     {
         var state = getTopState();
-        var x2 = x + width;
-        var y2 = y + height;
-        var scratch = _scratchQuadVector;
 
-        scratch[0] = x;
-        scratch[1] = y;
-        // scratch[2] = 0;
-
-        scratch[3] = x2;
-        scratch[4] = y;
-        // scratch[5] = 0;
-
-        scratch[6] = x2;
-        scratch[7] = y2;
-        // scratch[8] = 0;
-
-        scratch[9] = x;
-        scratch[10] = y2;
-        // scratch[11] = 0;
-
-        state.matrix.transformVectors(scratch, scratch);
-
-        var offset = _batcher.prepareFillRect(_renderTarget, state.blendMode);
-        var data = _batcher.data;
+        var pos = transformQuad(x, y, width, height);
         var r = (color & 0xff0000) / 0xff0000;
         var g = (color & 0x00ff00) / 0x00ff00;
         var b = (color & 0x0000ff) / 0x0000ff;
         var a = state.alpha;
 
-        data[  offset] = scratch[0];
-        data[++offset] = scratch[1];
+        var offset = _batcher.prepareFillRect(_renderTarget, state.blendMode);
+        var data = _batcher.data;
+
+        data[  offset] = pos[0];
+        data[++offset] = pos[1];
         data[++offset] = r;
         data[++offset] = g;
         data[++offset] = b;
         data[++offset] = a;
 
-        data[++offset] = scratch[3];
-        data[++offset] = scratch[4];
+        data[++offset] = pos[3];
+        data[++offset] = pos[4];
         data[++offset] = r;
         data[++offset] = g;
         data[++offset] = b;
         data[++offset] = a;
 
-        data[++offset] = scratch[6];
-        data[++offset] = scratch[7];
+        data[++offset] = pos[6];
+        data[++offset] = pos[7];
         data[++offset] = r;
         data[++offset] = g;
         data[++offset] = b;
         data[++offset] = a;
 
-        data[++offset] = scratch[9];
-        data[++offset] = scratch[10];
+        data[++offset] = pos[9];
+        data[++offset] = pos[10];
         data[++offset] = r;
         data[++offset] = g;
         data[++offset] = b;
@@ -309,6 +249,32 @@ class Stage3DGraphics
     inline private function getTopState () :DrawingState
     {
         return _stateList;
+    }
+
+    private function transformQuad (x :Float, y :Float, width :Float, height :Float) :Vector<Float>
+    {
+        var x2 = x + width;
+        var y2 = y + height;
+        var pos = _scratchQuadVector;
+
+        pos[0] = x;
+        pos[1] = y;
+        // pos[2] = 0;
+
+        pos[3] = x2;
+        pos[4] = y;
+        // pos[5] = 0;
+
+        pos[6] = x2;
+        pos[7] = y2;
+        // pos[8] = 0;
+
+        pos[9] = x;
+        pos[10] = y2;
+        // pos[11] = 0;
+
+        getTopState().matrix.transformVectors(pos, pos);
+        return pos;
     }
 
     private static var _scratchMatrix3D = new Matrix3D();

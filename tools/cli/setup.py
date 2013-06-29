5b96804d13671466bc5d6e2e22bf9939ba693a03
Bruno Garcia <b@aduros.com>
2013-06-29 14:11:11 -0700

    Specify dependencies.

--- tools/cli/setup.py
+++ tools/cli/setup.py
@@ -2,8 +2,17 @@
 
 from setuptools import setup, find_packages
 import os
+import sys
 
 VERSION="3.0.0"
+
+install_requires = [
+    "PyYAML >= 3.10",
+    "Twisted >= 12.3.0",
+]
+if sys.version_info < (2, 7, 0):
+    # argparse is already included with 2.7+
+    install_requires.append("argparse >= 1.1")
 
 data = []
 for root, dirs, files in os.walk("src/flambe/data"):
@@ -16,6 +25,7 @@ setup(name="flambe", version=VERSION,
     package_data={"flambe": data},
     scripts=["scripts/flambe"],
     zip_safe=False,
+    install_requires=install_requires,
 
     author="Bruno Garcia",
     author_email="b@aduros.com",

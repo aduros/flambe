95e96b035a93801d780bc32ba8a3df293b43c218
Bruno Garcia <b@aduros.com>
2013-06-29 01:45:47 -0700

    Properly recursively include everything in the data dir.

--- tools/cli/setup.py
+++ tools/cli/setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
 
 VERSION="3.0.0"
+
+data = []
+for root, dirs, files in os.walk("src/flambe/data"):
+    data += ["%s/%s" % (os.path.relpath(root, "src/flambe"), file) for file in files]
 
 setup(name="flambe", version=VERSION,
 
     packages=find_packages("src"),
     package_dir={"": "src"},
-    package_data = {"flambe": ["data/*"]},
+    package_data={"flambe": data},
     scripts=["scripts/flambe"],
     zip_safe=False,
 

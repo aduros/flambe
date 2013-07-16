a2babe4c9b31000d40186ab285a3d70e9abfe667
Bruno Garcia <b@aduros.com>
2013-07-16 13:03:00 -0700

    Haxe 3 (already out of date!) doesn't support recursive constraints.

--- src/flambe/platform/BasicAsset.hx
+++ src/flambe/platform/BasicAsset.hx
@@ -8,7 +8,7 @@ import flambe.asset.Asset;
 import flambe.util.Assert;
 import flambe.util.Value;
 
-class BasicAsset<A:BasicAsset<A>>
+class BasicAsset<A>
     implements Asset
 {
     public var reloadCount (get, null) :Value<Int>;

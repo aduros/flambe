559f1f3174d3f95f8304ad2f11c62416f5745968
Bruno Garcia <b@aduros.com>
2013-08-26 12:13:12 -0700

    @:keep Scene's constructor to workaround reflection issues.
    I'd like to investigate this further, the fact that this is necessary
    suggests a Haxe bug... It seems like if you @:keep your CustomScene
    constructor, and it calls super(), then things should just work.
    
    Refs #162.

--- src/flambe/scene/Scene.hx
+++ src/flambe/scene/Scene.hx
@@ -24,7 +24,7 @@ class Scene extends Component
      */
     public var opaque (default, null) :Bool;
 
-    public function new (opaque :Bool = true)
+    @:keep public function new (opaque :Bool = true)
     {
         this.opaque = opaque;
         shown = new Signal0();

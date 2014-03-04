a67792183dbc0fb4116f80f6dc6e25961b2bc4c8
Bruno Garcia <b@aduros.com>
2014-03-04 14:25:50 -0500

    Doc improvements.

--- src/flambe/util/Value.hx
+++ src/flambe/util/Value.hx
@@ -15,7 +15,7 @@ import flambe.util.Signal2;
 class Value<A>
 {
     /**
-     * The wrapped value, setting this to a different value will fire the 'changed' signal.
+     * The wrapped value, setting this to a different value will fire the `changed` signal.
      */
     public var _ (get, set) :A;
 

651ac97858839e6e5cef4b70909ebaa5dfdf4e24
Jason Knobler <jayknobler@gmail.com>
2014-04-03 23:11:31 -0400

    New drawing API, (WIP)

--- src/flambe/display/Shape.hx
+++ src/flambe/display/Shape.hx
@@ -4,9 +4,17 @@
 
 package flambe.display;
 
+import flambe.display.Shape.LineCaps;
 import flambe.display.Sprite;
 import flambe.math.Point;
+import flambe.math.FMath;
 import flambe.util.Assert;
+
+enum LineCaps
+{
+    None;
+    Rounded;
+}
 
 /**
  * A user defined shape (line, rectangle, polygon) that is assembled by adding
@@ -14,81 +22,135 @@ import flambe.util.Assert;
  */
 class Shape extends Sprite
 {
-    public var color(default, default) :Int;
-    private var segments :Array<Segment>;
+    public var lineWidth(default, default) :Float;
+    public var lineCap(default, default) :LineCaps;
+    public var strokeColor(default, default) :Int;
+    public var fillColor(default, default) :Int;
+    public var penCoordinate(default, null) :Point;
+    
+    private var _segments :Array<Segment>;
+   
 
     public function new()
     {
         super();
-
-        color = 0x000000;
-        segments = new Array<Segment>();
+        
+        lineWidth = 1.0;
+        lineCap = None;
+        strokeColor = 0x000000;
+        fillColor = 0xFFFFFF;
+        penCoordinate = new Point();
+        
+        _segments = new Array<Segment>();
     }
 
-    /**
-     * Adds a line segment to this shape. The coordinates specified are local to the Shape's origin.
-     * @returns This instance, for chaining.
-     */
-    public function addLineSegmentF(xStart :Float, yStart :Float, xEnd :Float, yEnd :Float, width :Float, ?roundedCap :Bool = false) :Shape
+    public function lineStyle(width :Float, color :Int, cap :LineCaps) : Void
     {
-        var index = segments.length;
-        segments[index] = new Segment(xStart, yStart, xEnd, yEnd, width, roundedCap);
-
-        return this;
+        lineWidth = width;
+        strokeColor = color;
+        lineCap = cap;
     }
-
-    /**
-     * Adds a line segment to this shape. The coordinates specified are local to the Shape's origin.
-     * @returns This instance, for chaining.
-     */
-    public function addLineSegment(ptStart :Point, ptEnd :Point, width :Float, ?roundedCap :Bool = false) :Shape
+    
+    public function fillStyle(color :Int) : Void
     {
-        var index = segments.length;
-        segments[index] = new Segment(ptStart.x, ptStart.y, ptEnd.x, ptEnd.y, width, roundedCap);
-
-        return this;
+        fillColor = color;
     }
-
-    /**
-     * Adds a contiguous line strip to this shape. The coordinates specified are local to the Shape's origin.
-     * @returns This instance, for chaining.
-     */
-    public function addLineStrip(ptArray :Array<Point>, width :Float, ?roundedCap :Bool = false)
+    
+    public function moveTo(x :Float, y :Float) : Void
     {
-        Assert.that(ptArray.length >= 2, "addLineStrip() must have at least '2' Points");
+        penCoordinate.set(x, y);
+    }
+    
+    public function lineTo(x :Float, y :Float) : Void
+    {
+        var startPoint :Point = new Point(penCoordinate.x, penCoordinate.y);
+        penCoordinate.set(x, y);
+        
+        trace(startPoint);
+        
+        var index = _segments.length;
+        _segments[index] = new Segment(startPoint, penCoordinate, lineWidth, lineCap, strokeColor);
+    }
+    
+    public function curveTo(anchorX :Float, anchorY :Float, x :Float, y :Float) : Void
+    {
+        // Determine how much percision
+        var iPercInterval = 0.1;    // 0.1 == 10 vertices
+        
+        var i :Float = 0.0;
+        var xa, ya, xb, yb;
+        while (i < 1.0) {
+            // Compute anchor/path
+            xa = FMath.lerp(penCoordinate.x, anchorX, i);
+            ya = FMath.lerp(penCoordinate.y, anchorY, i);
+            xb = FMath.lerp(anchorX, x, i );
+            yb = FMath.lerp(anchorY, y, i );
 
-        for(i in 1...ptArray.length) {
-            addLineSegment(ptArray[i - 1], ptArray[i], width, roundedCap);
+            // Find position along the anchor/path
+            lineTo(FMath.lerp(xa, xb, i), FMath.lerp(ya, yb, i));
+            
+            i += iPercInterval;
         }
+    }
+    
+    public function drawCircle(x :Float, y :Float, radius :Float) : Void
+    {
+        var numWedges :Int = Std.int(radius / 2);
+        if (numWedges < 12) numWedges = 12;
 
-        return this;
+        var wedgeAngle :Float = (2.0 * Math.PI) / numWedges;
+        
+        moveTo(x + radius, y);
+        var theta :Float = 0.0;
+        for (i in 0...numWedges)
+        {
+            theta = i * wedgeAngle;
+            lineTo(x + radius * Math.cos(theta), y + radius * Math.sin(theta));
+        }
+    }
+    
+    public function drawEllipse(x :Float, y :Float, width :Float, height :Float, ?rotation :Float) : Void
+    {
+    }
+    
+    public function drawRect(x :Float, y :Float, width :Float, height :Float) : Void
+    {
+        moveTo(x, y);
+        lineTo(x + width, y);
+        lineTo(x + width, y + height);
+        lineTo(x, y + height);
+        lineTo(x, y);
+    }
+    
+    public function clear() : Void
+    {
+        _segments = new Array<Segment>();
     }
 
     override public function draw (g :Graphics)
     {
-        for (seg in segments) {
-            seg.draw(g, color);
+        for (seg in _segments) {
+            g.drawLine(seg.color, seg.startPt.x, seg.startPt.y, seg.endPt.x, seg.endPt.y, seg.width, seg.cap == Rounded);
         }
     }
 }
 
 private class Segment
 {
-    public var ptStart :Point;
-    public var ptEnd :Point;
-    public var width :Float;
-    public var roundedCap :Bool;
+    public var startPt(default, null) :Point;
+    public var endPt(default, null) :Point;
+    public var width(default, null) :Float;
+    public var cap(default, null) :LineCaps;
+    public var color(default, null) :Int;
 
-    public function new(xStart :Float, yStart :Float, xEnd :Float, yEnd :Float, width :Float, roundedCap :Bool)
+    public function new(startPoint :Point, endPoint :Point, lineWidth :Float, lineCap :LineCaps, clr :Int)
     {
-        ptStart = new Point(xStart, yStart);
-        ptEnd = new Point(xEnd, yEnd);
-        this.width = width;
-        this.roundedCap = roundedCap;
-    }
-
-    public function draw (g :Graphics, color :Int)
-    {
-        g.drawLine(color, ptStart.x, ptStart.y, ptEnd.x, ptEnd.y, width, roundedCap);
+        startPt = new Point(startPoint.x, startPoint.y);
+        endPt = new Point(endPoint.x, endPoint.y);
+        
+        width = lineWidth;
+        cap = lineCap;
+        
+        color = clr;
     }
 }

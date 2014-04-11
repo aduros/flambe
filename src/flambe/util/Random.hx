41654685c47eec191100616da8a3fc3a7019ba34
Mark Knol <mark.knol@gmail.com>
2014-04-11 10:07:09 +0200

    removed getter/setter, added reset instead

--- src/flambe/util/Random.hx
+++ src/flambe/util/Random.hx
@@ -12,15 +12,9 @@ import flambe.math.FMath;
  */
 class Random
 {
-    /**
-     * The original used seed. When seed is set, internal state will reset to give the ability 
-     * to run the same sequence of random numbers again.
-     */
-    public var seed(get, set) :Int;
-
     public function new (?seed :Int)
     {
-        this.seed = (seed != null) ? seed : Math.floor(Math.random() * FMath.INT_MAX);
+        _state = (seed != null) ? seed : Math.floor(Math.random() * FMath.INT_MAX);
     }
 
     /**
@@ -42,17 +36,10 @@ class Random
         return nextInt() / FMath.INT_MAX;
     }
 
-    inline private function get_seed() :Int
+    public function reset(value :Int)
     {
-        return _seed;
-    }
-
-    inline private function set_seed(value :Int)
-    {
-        _seed = _state = value;
-        return value;
+        _state = value;
     }
 
     private var _state :Int;
-    private var _seed :Int;
 }

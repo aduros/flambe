10e6150dda74594d78d928a665d4ebbd2990686a
Mark Knol <mark.knol@gmail.com>
2014-06-26 09:48:35 +0200

    Tween.hx delay + onComplete callback

--- src/flambe/animation/Tween.hx
+++ src/flambe/animation/Tween.hx
@@ -11,33 +11,44 @@ class Tween
 {
     public var elapsed (default, null) :Float;
 
-    public function new (from :Float, to :Float, seconds :Float, ?easing :EaseFunction)
+    public function new (from :Float, to :Float, seconds :Float, ?easing :EaseFunction, ?onComplete :Void->Void, delay :Float = 0)
     {
         _from = from;
         _to = to;
         _duration = seconds;
         elapsed = 0;
         _easing = (easing != null) ? easing : Ease.linear;
+        _onComplete = onComplete;
+        _delay = delay;
     }
 
     public function update (dt :Float) :Float
     {
         elapsed += dt;
-
-        if (elapsed >= _duration) {
+        
+        if ((elapsed - _delay) >= _duration) {
+            if (_onComplete != null) 
+            {
+                _onComplete(); 
+                _onComplete = null;
+            }
             return _to;
+        } else if (elapsed > _delay) {
+            return _from + (_to - _from) * _easing((elapsed - _delay) / _duration);
         } else {
-            return _from + (_to - _from)*_easing(elapsed/_duration);
+            return _from;
         }
     }
 
     public function isComplete () :Bool
     {
-        return elapsed >= _duration;
+        return (elapsed - _delay) >= _duration;
     }
 
     private var _from :Float;
     private var _to :Float;
     private var _duration :Float;
     private var _easing :EaseFunction;
+    private var _onComplete :Void->Void;
+    private var _delay :Float;
 }

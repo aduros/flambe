5728d6fbd5b4dcfee027e77694d26fdf9c275617
Jak Boulton <jak.boulton@gmail.com>
2015-04-23 10:03:25 +0100

    Added support for singular float and int attribute types (HXSL).

--- src/flambe/platform/shader/ShaderHXSL.hx
+++ src/flambe/platform/shader/ShaderHXSL.hx
@@ -15,73 +15,101 @@ interface ShaderHXSL
     public function setTexture(texture :TextureBase) :Void;
 }
 
+/** Utility class for managing custom attributes */
 class ShaderConst
 {
     public function new()
     {
-        _floatMap = new Map<String, Vector3D>();
-        _intMap = new Map<String, Vector3D>();
+        _floatMap = new Map<String, Float>();
+        _intMap = new Map<String, Int>();
+        _vectorMap = new Map<String, Vector3D>();
         _textureMap = new Map<String, TextureBase>();
     }
 
     /** Unility functions used to set custom attributes */
     public function setInt(name :String, value :Int) :Void
     {
+        var location = _intMap.get(name);
 
+        if(location != null)
+        {
+            location = value;
+        }
     }
 
     public function setFloat(name :String, x :Float) :Void
     {
-        var value = _floatMap.get(name);
+        var location = _floatMap.get(name);
 
-        if(value != null)
+        if(location != null)
         {
-            value.x = x;
+            location = x;
         }
     }
 
     public function setFloat2(name :String, x :Float, y :Float) :Void
     {
-        var value = _floatMap.get(name);
+        var location = _vectorMap.get(name);
 
-        if(value != null)
+        if(location != null)
         {
-            value.x = x;
-            value.y = y;
+            location.x = x;
+            location.y = y;
         }
     }
 
     public function setFloat3(name :String,  x :Float, y :Float, z :Float) :Void
     {
-        var value = _floatMap.get(name);
+        var location = _vectorMap.get(name);
 
-        if(value != null)
+        if(location != null)
         {
-            value.x = x;
-            value.y = y;
-            value.z = z;
+            location.x = x;
+            location.y = y;
+            location.z = z;
         }
     }
 
     public function setFloat4(name :String, x :Float, y :Float, z :Float, w :Float) :Void
     {
-        var value = _floatMap.get(name);
+        var location = _vectorMap.get(name);
 
-        if(value != null)
+        if(location != null)
         {
-            value.x = x;
-            value.y = y;
-            value.z = z;
-            value.w = w;
+            location.x = x;
+            location.y = y;
+            location.z = z;
+            location.w = w;
         }
     }
 
-    public function linkUniformf(name :String, value :Vector3D)
+    public function linkUniform1f(name :String, value :Float)
     {
         _floatMap.set(name, value);
     }
 
-    private var _floatMap :Map<String, Vector3D>;
-    private var _intMap :Map<String, Vector3D>;
+    public function linkUniform2f(name :String, value :Vector3D)
+    {
+        _vectorMap.set(name, value);
+    }
+
+    public function linkUnifor3f(name :String, value :Vector3D)
+    {
+        _vectorMap.set(name, value);
+    }
+
+    public function linkUniform4f(name :String, value :Vector3D)
+    {
+        _vectorMap.set(name, value);
+    }
+
+    public function linkUniformi(name :String, value :Int)
+    {
+        _intMap.set(name, value);
+    }
+
+    private var _floatMap   :Map<String, Float>;
+    private var _intMap     :Map<String, Int>;
+    private var _vectorMap  :Map<String, Vector3D>;
     private var _textureMap :Map<String, TextureBase>;
 }
